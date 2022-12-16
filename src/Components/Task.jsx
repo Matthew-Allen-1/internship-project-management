22818b092b8059ebc0642104462e6359af0ad931
Matthew Allen <math.d.allen@gmail.com>
2022-12-16 12:56:46 -0800

    Adds new taskData state and converts lists of tasks from groupData objects to lists of task ids

--- src/Components/Task.jsx
+++ src/Components/Task.jsx
@@ -6,17 +6,17 @@ export default function Task(props){
   //displays information about each task
   return(
     <div className="created-task">
-      <input className="input" defaultValue={props.taskData.title} type="text" />
+      <input className="input" defaultValue={props.task.title} type="text" />
       <ul>
-        <li>{props.taskData.group}</li>
+        <li>{props.task.groupTitle}</li>
       </ul>
       <span className="line-divider"></span>
       <div className="time">
-        <input defaultValue = {props.taskData.startTime} type="time" />
+        <input defaultValue = {props.task.startTime} type="time" />
         <div className="time-divider">-</div>
-        <input defaultValue = {props.taskData.endTime} type="time" />
+        <input defaultValue = {props.task.endTime} type="time" />
       </div>
-      <input className="date" type="date" defaultValue= {props.taskData.date}/>
+      <input className="date" type="date" defaultValue= {props.task.date}/>
       <span className="line-divider"></span>
       <img className="options" src="https://app.clockify.me/assets/ui-icons/menu-dots-vertical.svg" alt="" />
     </div>
