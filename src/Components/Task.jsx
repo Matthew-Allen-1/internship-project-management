87915a8c31b00da489ab7d7ed21148d75f9d38ea
dluangrath <dluangrath9@gmail.com>
2022-12-20 00:38:22 -0800

    added options dropdown in Task component

--- src/Components/Task.jsx
+++ src/Components/Task.jsx
@@ -18,7 +18,22 @@ export default function Task(props){
       </div>
       <input className="date" type="date" defaultValue= {props.task.date}/>
       <span className="line-divider"></span>
-      <img className="options" src="https://app.clockify.me/assets/ui-icons/menu-dots-vertical.svg" alt="" />
+      {/* <img className="options" src="https://app.clockify.me/assets/ui-icons/menu-dots-vertical.svg" alt="" /> */}
+
+{/* option drop down */}
+<div className="menu-nav">
+        <div className="menu-item">Group Item</div>
+        <div className="dropdown-container" tabIndex="-1">
+          <div className="three-dots"></div>
+          <div className="dropdown">
+            <a href="#"><div>dropdown 1</div></a>
+            <a href="#"><div>dropdown 2</div></a>
+            <a href="#"><div>dropdown 3</div></a>
+          </div>
+        </div>
+      </div> 
+
+
     </div>
   )
 }
